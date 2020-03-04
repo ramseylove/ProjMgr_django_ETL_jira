a217b66df15b7371d7e8f9db85c1375567bc0b82
Ryan M <ryan@atriadev.com>
2020-03-03 17:19:46 -0700

    started work on models for project_tracking

--- project_tracking/models.py
+++ project_tracking/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    id = models.IntegerField(primary_key=True, max_length=20, unique=True)
+    key = models.CharField(primary_key=True, max_length=20, unique=True)
+    name = models.CharField(max_length=155)
+    category_id = models.IntegerField(max_length=20)
+    category_name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Issue(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True, max_length=20)
+    key = models.CharField(max_length=10)
+    summary = models.Charfield(max_length=300)
+    description = models.TextField()
+    status_change_date = models.DateTimeField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+    status =  models.CharField(max_length=30)
+
+    project_id = models.ForeignKey("Project", on_delete=models.CASCADE))
+    issue_type_id = models.ForeignKey("IssueTypes", null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+class IssueTypes(models.Model):
+    id = models.IntegerField(primary_key=True, unique=true, max_length=20)
+    name = models.CharField(max_length=100)
+
+    project_id = models.ForeignKey("Project", null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
